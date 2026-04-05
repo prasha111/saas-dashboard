c08a6acb89af7e3cd1d1692c14d0285f7bf1f441
prasha111 <prashantsharma556@gmail.com>
2026-04-05 16:13:53 +0530

    file structure

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export async function getServerSideProps() {
+    return {
+      props: {
+        message: "SSR Working",
+      },
+    };
+  }
+  
+  export default function Home({ message }) {
+    return <h1>{message}</h1>;
+  }
